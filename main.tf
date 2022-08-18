f1e4a29a10dd6896f968df242183bd14d9dbcbbe
Norman Pellet <norman.pellet@gmail.com>
2022-08-18 13:40:17 +0200

    Refactor module definition

--- main.tf
+++ main.tf
@@ -1,23 +1,47 @@
 
 
 provider "aws" {
-  region="eu-central-1"
+  region = "eu-central-1"
 }
 
+variable "lambda_name" {
+  type        = string
+  description = "The name of the lambda image"
+}
+
+variable "lambda_ref" {
+  type        = string
+  description = "The reference (tag or branch) of the lambda"
+}
+
+variable "handler" {
+  type        = string
+  description = "The handler function (path/to/main.functioname)"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of your lambda"
+}
+
+variable "description" {
+  type    = string
+  default = ""
+}
 
 module "lambda" {
   source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "my-lambda1"
-  description   = "My awesome lambda function"
-  handler       = "main.handler"
+  function_name = vars.name
+  description   = vars.description
+  handler       = vars.handler
   runtime       = "nodejs16.x"
-  
+
   create_package = false
-   s3_existing_package = {
+
+  s3_existing_package = {
     bucket = "zipped-lambda-artifacts-441772730001"
-    key    = "NPellet/terraform-lambda-aws-test/879060db2eaa437ed18d06e016a3918cb9f72cd7.zip"
+    key    = "${var.lambda_name}.${var.lambda_ref}.zip"
   }
-  
 }
 
